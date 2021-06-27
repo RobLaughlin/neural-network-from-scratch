d4a2797c7d8bfb5e894990c85fcb9a6502be2b31
RobLaughlin <waterfallkicks@gmail.com>
2021-06-26 21:19:42 -0700

    cleanup network model

--- init_network.py
+++ init_network.py
@@ -1,36 +1,29 @@
 from PIL import Image
+import numpy as np
 from network import Layer, NeuralNetwork, sigmoid
 from data_parser import DataParser, ImageData
 
-def format_image_data(imgdata:ImageData, index):
+def format_image_data(imgdata:ImageData, index, scaledown):
     # Just pick 1 of the RGB values, it's grayscale so it doesn't matter which one.
     # We also format the matrix image data into a column vector with size equal to
     # the number of pixels.
-    return imgdata.images[index][:, :, 0].flatten().reshape(imgdata.img_width * imgdata.img_height, 1)
-
-def generate_layers(imgdata):
-    
-    # Initial layer
-    layer0_neuron_values = format_image_data(imgdata, 0)
-    layer0 = Layer(neurons=len(layer0_neuron_values), prevalues=layer0_neuron_values)
-
-    # Hidden layers
-    layer1 = Layer(neurons=imgdata.img_width, prevalues=layer0.values)
-    layer2 = Layer(neurons=imgdata.img_height, prevalues=layer1.values)
-
-    # Output layer
-    layer3 = Layer(neurons=10, prevalues=layer2.values)
-    layers = [layer0, layer1, layer2, layer3]
-
-    return layers
+    return imgdata.images[index][:, :, 0].flatten() / scaledown
 
 if __name__ == '__main__':
+    scaledown = 50
+
     imgdata_filepath = 'data/train-images.idx3-ubyte'
     labeldata_filepath = 'data/train-labels.idx1-ubyte'
     imgdata = DataParser.parse_training_image_file(imgdata_filepath)
     labeldata = DataParser.parse_training_label_file(labeldata_filepath)
 
-    layers = generate_layers(imgdata)
-    network = NeuralNetwork(layers)
     
+    values = format_image_data(imgdata, 0, scaledown)
+    layers = np.array([len(values), 28, 28, 10])
+
+    y= np.zeros(shape=(10, 1))
+    y[4] = 1
+
+    network = NeuralNetwork(layers=layers)
+    network.load_data(values, y)
 
--- network.py
+++ network.py
@@ -1,6 +1,7 @@
+from abc import ABC, abstractclassmethod
 import numpy as np
 
-def sigmoid(Z:np.array):
+def sigmoid(Z: np.array):
     """
     Z ~ The compact form for the weighted sum (W @ L(n-1) + b(n))
     """
@@ -16,41 +17,91 @@ def d_sigmoid(Z:np.array):
     Z -= np.square(Z)
     return Z
 
-class Layer:
-    def __init__(self, neurons:int, prevalues:np.array, weights:np.matrix=None, biases:np.array=None, sigma=sigmoid, dsigma=d_sigmoid):
+class Layer(ABC):
+    def __init__(self, neurons: int, sigma=sigmoid, dsigma=d_sigmoid):
         """
-        neurons     ~ the number of neurons in the layer
-        prevalues   ~ the values of each neuron in the previous layer (or initial supplied data if this is the first layer)
-        sigma       ~ the activation function to normalize the weighted sums
-        dsigma      ~ the derivative of the sigmoid function
-        weights     ~ a (N(L) x N(L-1)) matrix of weights, where N(L) is the number of neurons in layer L.
-        biases      ~ a N(L) vector of float64s.
+        neurons     ~ The number of neurons in the layer
+        sigma       ~ The differentiable function to apply to the neurons
+        dsigma      ~ The derivative of the sigma function
         """
-
-        self.neurons = neurons
-        prev_layer_neurons = len(prevalues)
-        xavier_multiplier = np.sqrt(1/(prev_layer_neurons + neurons))
-
-        if weights == None:
-            weights = np.random.randn(neurons, prev_layer_neurons).astype(np.float64) * xavier_multiplier
         
-        if biases == None:
-            biases = np.zeros(shape=(neurons, 1))
+        self.neurons    = neurons
+        self.sigma      = sigma
+        self.dsigma     = dsigma
+
+        self.values     = np.zeros(neurons, dtype=np.float64)
+        self.Z          = np.zeros(neurons, dtype=np.float64) # Shorthand for the weighted sum before being passed through the sigma function
+
+class InputLayer(Layer):
+    def __init__(self, neurons: int, sigma=sigmoid, dsigma=d_sigmoid):
+        super().__init__(neurons, sigma=sigma, dsigma=dsigma)
+    
+    def update_values(self, values: np.array):
+        self.values = self.sigma(values)
+        self.Z = values
+    
+class WeightedLayer(Layer):
+    def __init__(self, neurons: int, prevLayer:Layer, sigma=sigmoid, dsigma=d_sigmoid):
+        super().__init__(neurons, sigma=sigma, dsigma=dsigma)
         
-        self.values = sigma((weights @ prevalues) + biases)
-        self.weights = weights
-        self.biases = biases
-        self.sigma = sigma
-        self.dsigma = dsigma
+        L0 = len(prevLayer.values)
+        L1 = neurons
+        xavier_multiplier = np.sqrt(1 / (L0 + L1))
+
+        self.weights = np.random.randn(L0, L1) * xavier_multiplier
+        self.weights_T = self.weights.T
+        self.bias = np.zeros(L1).T
+
+        self.partial_weights = np.zeros_like(self.weights)
+        self.partial_bias = np.zeros_like(self.bias)
+    
+    def update_values(self, prevLayer: Layer):
+        self.Z = (self.weights_T @ prevLayer.values) + self.bias
+        self.values = self.sigma(self.Z)
     
 class NeuralNetwork:
-    def __init__(self, layers:list):
+    def __init__(self, layers: np.array):
         """
-        layers is an ordinary python list of Layers where
-        layers[0] is the input layer,
-        layers[1...L-1] are the hidden layers,
-        layers[L] is the output layer.
+        layers is an array of integers, where
+        layers[0] is the number of neurons in the input layer,
+        layers[1,2,...L-1] is the number of neurons in each respective hidden layer, and
+        layers[L] is the number of neurons in the output layer.
         """
-        self.layers = layers
-        pass
 
+        # Must have at least 1 input and 1 output layer in the network
+        assert(len(layers) >= 2)
+
+        self.layers = list()
+
+        input_layer = InputLayer(layers[0])
+        self.layers.append(input_layer)
+
+        for l in range(1, len(layers)):
+            layer = WeightedLayer(layers[l], self.layers[l - 1])
+            self.layers.append(layer)
+        
+        # X is the unscaled input data vector
+        self.X = None
+
+        # Y is the expected output vector
+        self.Y = None
+    
+    def load_data(self, X: np.array, Y: np.array):
+        assert(self.layers[0].neurons  == len(X))
+        assert(self.layers[-1].neurons == len(Y))
+        self.X = X
+        self.Y = Y
+
+        self.layers[0].update_values(X)
+        self.feed_forward()
+    def cost(self):
+        last_layer = self.layers[-1]
+        V = (last_layer.values - self.Y).flatten()
+        return np.dot(V, V)
+    
+    def feed_forward(self):
+        for l in range(1, len(self.layers)):
+            self.layers[l].update_values(self.layers[l - 1])
+    
+    def backpropogate(self):
+        pass

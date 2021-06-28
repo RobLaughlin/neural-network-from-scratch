0eb4b89993edf4bbbede9e8ab84a140cb84c113c
RobLaughlin <waterfallkicks@gmail.com>
2021-06-27 23:23:27 -0700

    Implement backprop and SGD

--- init_network.py
+++ init_network.py
@@ -1,7 +1,6 @@
-from PIL import Image
 import numpy as np
-from network import Layer, NeuralNetwork, sigmoid
-from data_parser import DataParser, ImageData
+from network import NeuralNetwork, reLU, d_reLU, sigmoid, d_sigmoid
+from data_parser import DataParser, ImageData, LabelData
 
 def format_image_data(imgdata:ImageData, index, scaledown):
     # Just pick 1 of the RGB values, it's grayscale so it doesn't matter which one.
@@ -9,21 +8,47 @@ def format_image_data(imgdata:ImageData, index, scaledown):
     # the number of pixels.
     return imgdata.images[index][:, :, 0].flatten() / scaledown
 
-if __name__ == '__main__':
-    scaledown = 50
+def format_label_data(labeldata:LabelData, index):
+    label_vec = np.zeros(10, dtype=np.uint8)
+    label_vec[labeldata.labels[index]] = 1
+    return label_vec
 
+def generate_training_examples(imgdata: ImageData, labeldata: LabelData):
+    assert(imgdata.num_images == labeldata.num_labels)
+    training_examples = list()
+    for T in range(imgdata.num_images):
+        (img, label) = (format_image_data(imgdata, T, scaledown), format_label_data(labeldata, T))
+        training_examples.append((img, label))
+    
+    return training_examples
+
+if __name__ == '__main__':
+    scaledown = 255
+    mini_batch_size = 15
+    eta = 0.75
+    epochs = 20
+    num_outputs = 10
+
+    # Training data
     imgdata_filepath = 'data/train-images.idx3-ubyte'
     labeldata_filepath = 'data/train-labels.idx1-ubyte'
     imgdata = DataParser.parse_training_image_file(imgdata_filepath)
     labeldata = DataParser.parse_training_label_file(labeldata_filepath)
 
+    # Test data
+    test_imgdata_filepath = 'data/testdata/t10k-images.idx3-ubyte'
+    test_labeldata_filepath = 'data/testdata/t10k-labels.idx1-ubyte'
+    test_imgdata = DataParser.parse_training_image_file(test_imgdata_filepath)
+    test_labeldata = DataParser.parse_training_label_file(test_labeldata_filepath)
+
+    training_examples = generate_training_examples(imgdata, labeldata)
+    test_training_examples = generate_training_examples(test_imgdata, test_labeldata)
+
+    layers = np.array([
+        (imgdata.img_width * imgdata.img_height, sigmoid, d_sigmoid), 
+        (imgdata.img_width, sigmoid, d_sigmoid), 
+        (imgdata.img_height, sigmoid, d_sigmoid), 
+        (num_outputs, sigmoid, d_sigmoid)])
     
-    values = format_image_data(imgdata, 0, scaledown)
-    layers = np.array([len(values), 28, 28, 10])
-
-    y= np.zeros(10)
-    y[4] = 1
-
     network = NeuralNetwork(layers=layers)
-    network.load_data(values, y)
-
+    network.SGD(training_examples, test_training_examples, mini_batches=mini_batch_size, eta=eta, epochs=epochs)
--- network.py
+++ network.py
@@ -1,24 +1,35 @@
 from abc import ABC, abstractclassmethod
 import numpy as np
+import random
 
 def sigmoid(Z: np.array):
     """
     Z ~ The compact form for the weighted sum (W @ L(n-1) + b(n))
     """
-    Z = 1 / (1 + np.exp(-Z))
+    Z = 1.0 / (1.0 + np.exp(-Z))
     return Z
 
-def d_sigmoid(Z:np.array):
+def d_sigmoid(Z: np.array):
     """
     Z ~ The compact form for the weighted sum (W @ L(n-1) + b(n))
     """
-    denom = np.exp(Z) + 1
-    Z = 1 / denom
-    Z -= np.square(Z)
-    return Z
+    return sigmoid(Z)*(1-sigmoid(Z))
+
+def reLU(Z: np.array):
+    """
+    Rectified linear unit applied to the weighted sum
+    """
+    return np.maximum(0, Z)
+
+def d_reLU(Z: np.array):
+    """
+    Piecewise derivative of reLU
+    """
+    return (Z > 0).astype(int)
+
 
 class Layer(ABC):
-    def __init__(self, neurons: int, sigma=sigmoid, dsigma=d_sigmoid):
+    def __init__(self, neurons: int, sigma, dsigma):
         """
         neurons     ~ The number of neurons in the layer
         sigma       ~ The differentiable function to apply to the neurons
@@ -33,7 +44,7 @@ class Layer(ABC):
         self.Z          = np.zeros(neurons, dtype=np.float64) # Shorthand for the weighted sum before being passed through the sigma function
 
 class InputLayer(Layer):
-    def __init__(self, neurons: int, sigma=sigmoid, dsigma=d_sigmoid):
+    def __init__(self, neurons: int, sigma, dsigma):
         super().__init__(neurons, sigma=sigma, dsigma=dsigma)
     
     def update_values(self, values: np.array):
@@ -41,13 +52,13 @@ class InputLayer(Layer):
         self.Z = values
     
 class WeightedLayer(Layer):
-    def __init__(self, neurons: int, prevLayer:Layer, sigma=sigmoid, dsigma=d_sigmoid):
+    def __init__(self, neurons: int, prevLayer:Layer, sigma, dsigma):
         super().__init__(neurons, sigma=sigma, dsigma=dsigma)
         
         L0 = prevLayer.neurons
         L1 = neurons
-        xavier_multiplier = np.sqrt(1 / (L0 + L1))
 
+        xavier_multiplier = 1 / np.sqrt(L0 + L1)
         self.weights = np.random.randn(L0, L1) * xavier_multiplier
         self.weights_T = self.weights.T
 
@@ -55,18 +66,31 @@ class WeightedLayer(Layer):
 
         self.partial_weights = np.zeros_like(self.weights)
         self.partial_bias = np.zeros_like(self.bias)
-    
+
     def update_values(self, prevLayer: Layer):
         self.Z = (self.weights_T @ prevLayer.values) + self.bias
         self.values = self.sigma(self.Z)
     
+    def reset_partials(self):
+        self.partial_weights = np.zeros_like(self.weights)
+        self.partial_bias = np.zeros_like(self.bias)
+    
+    def update_partials(self, pWeights: np.matrix, pBias: np.array):
+        self.partial_weights = self.partial_weights + pWeights
+        self.partial_bias = (self.partial_bias + pBias).flatten()
+    
+    def gradient_step(self, eta, mini_batch_size):
+        gradient_scalar = eta / mini_batch_size
+        self.weights -= (gradient_scalar * self.partial_weights)
+        self.weights_T = self.weights.T
+        self.bias -=(gradient_scalar * self.partial_bias)
+        self.reset_partials()
+
 class NeuralNetwork:
     def __init__(self, layers: np.array):
         """
-        layers is an array of integers, where
-        layers[0] is the number of neurons in the input layer,
-        layers[1,2,...L-1] is the number of neurons in each respective hidden layer, and
-        layers[L] is the number of neurons in the output layer.
+        layers is an array of 3-tuples, where
+        each tuple represents (number of neurons in respective index layer, sigma function, deriviative of respective sigma function)
         """
 
         # Must have at least 1 input and 1 output layer in the network
@@ -74,13 +98,13 @@ class NeuralNetwork:
 
         self.layers = list()
 
-        input_layer = InputLayer(layers[0])
+        input_layer = InputLayer(layers[0][0], layers[0][1], layers[0][2])
         self.layers.append(input_layer)
 
         for l in range(1, len(layers)):
-            layer = WeightedLayer(layers[l], self.layers[l - 1])
+            layer = WeightedLayer(layers[l][0], self.layers[l - 1], layers[l][1], layers[l][2])
             self.layers.append(layer)
-        
+
         # X is the unscaled input data vector
         self.X = np.zeros(input_layer.neurons)
 
@@ -95,8 +119,82 @@ class NeuralNetwork:
 
         self.layers[0].update_values(X)
         self.feed_forward()
-        self.backpropogate()
+    
+    def SGD(self, training_data: list, test_data: list, mini_batches: int, eta, epochs: int = 10):
+        """
+        Training data is a list of size T where
+        T ~ is the total number of training examples.
+
+        For each training example, we want 
+        T[0] ~ to be the input vector for our first training example, and
+        T[1] ~ to be the expected output vector for our cost function.
+
+        Test data should follow the same format as the training data.
+
+        mini_batches    ~ The size of each mini batch
+        eta             ~ The learning rate to use for gradient descent
+        epochs          ~ The number of times to run through all training examples
+        """
+
+        m = mini_batches
+        for e in range(epochs):
+            random.shuffle(training_data)
+            total_samples = len(training_data)
+
+            for ptr in range(total_samples // mini_batches):
+
+                batch = training_data[ptr*m : (ptr+1)*m]
+                for T in batch:
+                    X, Y = T[0], T[1]
+                    self.load_data(X, Y)
+                    self.backpropogate()
+                
+                for L in self.layers[1:]:
+                    L.gradient_step(eta, m)
+            
+            correct, tests = self.test_predictions(test_data)
+            print("Epoch: {0} ~ {1}/{2} correct predictions ({3:.2f}%).".format(e, correct, tests, correct / tests * 100))
+
+    def predict(self):
+        """
+        Return a tuple with
+        t[0] ~ the neuron value with the maximum output
+        t[1] ~ the index of the neuron value in the output layer
+        """
+        output_layer = self.layers[-1].values
+        mon_i = np.argmax(output_layer)
+        max_output_neuron = output_layer[mon_i]
         
+        return (max_output_neuron, mon_i)
+
+    def test_predictions(self, test_data: np.array):
+        """
+        Returns a ratio of correct predictions to tested predictions.
+
+        Test data is a list of size T where
+        T ~ is the total number of test examples.
+
+        For each test example, we want
+        T[0] ~ to be the input vector for our first training example, and
+        T[1] ~ to be the expected output vector for our cost function.
+        """
+
+        random.shuffle(test_data)
+
+        correct = 0
+        tested = 0
+        for T in test_data:
+            X, Y = T[0], T[1]
+            self.load_data(X, Y)
+            _, predicted_index = self.predict()
+
+            if Y[predicted_index] == 1:
+                correct += 1
+            
+            tested += 1
+        
+        return (correct, tested)
+    
     def cost(self):
         last_layer = self.layers[-1]
         V = (last_layer.values - self.Y).flatten()
@@ -112,8 +210,7 @@ class NeuralNetwork:
         
         # Delta of last layer
         delta = 2 * (ll.values - self.Y) * ll.dsigma(ll.Z)
-        ll.partial_bias = delta
-        ll.partial_weights = np.outer(self.layers[-2].values, ll.partial_bias)
+        ll.update_partials(np.outer(self.layers[-2].values, delta), delta)
 
         L = len(self.layers) - 2
         for i in range(L, 0, -1):
@@ -122,7 +219,6 @@ class NeuralNetwork:
             l = self.layers[i]
 
             delta = (nl.weights @ delta) * l.dsigma(l.Z)
-            l.partial_bias = delta
-            l.partial_weights = np.outer(pl.values, l.partial_bias)
+            l.update_partials(np.outer(pl.values, delta), delta)
 
 
